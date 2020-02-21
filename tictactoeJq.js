47026966367fddc3d698c176dc28a1e83050e0f3
abirsoni007 <60928776+abirsoni007@users.noreply.github.com>
2020-02-21 18:17:40 +0530

    Add files via upload

---
+++ tictactoeJq.js
@@ -0,0 +1,50 @@
+var x1=Math.random();
+var x =Math.round(x1);
+var firstplayername;
+var secondplayername;
+
+
+
+debugger
+function playerName(){
+  $("#startbutton").click(function(){
+      firstplayername = $("#fpn").val();
+      secondplayername = $("#spn").val();
+       
+    });
+
+  for (i = 0; i<3; i++){
+
+    if(x==1){
+      
+      $("#Name").html(firstplayername);
+      x=0;
+    }else{
+      
+      $("#Name").html(secondplayername);
+      x=1;  
+    }
+  }
+   switch (x){
+  
+    case 1:
+      ele = "X";
+      $("#val").html(ele);
+      break;    
+    case 0:
+      ele = "O";
+       
+      $("#val").html(ele);
+      break;
+   }
+}
+
+
+function print(buttonId){
+var i = buttonId;
+debugger
+  // //print the X or O
+  // document.getElementById(buttonId).innerHTML = ele;
+     $("#"+buttonId).html(ele);
+     playerName()
+   }
